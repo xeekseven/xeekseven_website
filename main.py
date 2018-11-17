98e720dfecb710326080942080d752777adf3d33
xeekseven <xeekseven@outlook.com>
2018-11-18 00:49:35 +0800

    修改init化函数，然后增加了nginx配置

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
+from xeekwebsite_server import app
 import xeekwebsite_server
+
 if __name__=='__main__':
-    app = xeekwebsite_server.creat_app()
+
+    #xeekwebsite_server.creat_app()
     app.run(host='0.0.0.0',port=5555)
